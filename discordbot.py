c27357f6e9cb920367d29d14d82d3fc375e2c1f3
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2018-02-14 17:13:10 -0700

    changed no u message

--- discordbot.py
+++ discordbot.py
@@ -5,14 +5,8 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import platform
 
+client = Bot(description="responds no u", command_prefix="", pm_help = False)
 
-message = "no u"
-
-# Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
-client = Bot(description="Basic Bot by Habchy#1665", command_prefix="", pm_help = False)
-
-# This is what happens everytime the bot launches. In this case, it prints information like server count, user count the bot is connected to, and the bot id in the console.
-# Do not mess with it because the bot can break, if you wish to do so, please consult me or someone trusted.
 @client.event
 async def on_ready():
 	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
@@ -29,21 +23,12 @@ async def on_ready():
 	print('Created by Habchy#1665')
 	return await client.change_presence(game=discord.Game(name='PLAYING STATUS HERE')) #This is buggy, let us know if it doesn't work.
 
-# This is a basic example of a call and response command. You tell it do "this" and it does it.
 @client.command()
 async def no(message):
     if(message == "u"):
-        await client.say("no u")
+        await client.say("false, 'tis indeed thou")
+    if(message == "me"):
+        await client.say("no me")
 
-# After you have modified the code, feel free to delete the line above so it does not keep popping up everytime you initiate the ping commmand.
 	
 client.run('NDEzNDY2MTQxMTg5NzM0NDMw.DWZSnw.W6Di4Rldu70f4SdyORmSvDmVaRE')
-
-# Basic Bot was created by Habchy#1665
-# Please join this Discord server if you need help: https://discord.gg/FNNNgqb
-# Please modify the parts of the code where it asks you to. Example: The Prefix or The Bot Token
-# This is by no means a full bot, it's more of a starter to show you what the python language can do in Discord.
-# Thank you for using this and don't forget to star my repo on GitHub! [Repo Link: https://github.com/Habchy/BasicBot]
-
-# The help command is currently set to be not be Direct Messaged.
-# If you would like to change that, change "pm_help = False" to "pm_help = True" on line 9.
