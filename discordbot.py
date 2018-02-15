14d06a538d0fa461375264f58a58bd44d930996c
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2018-02-15 14:46:03 -0700

    adfasdfsdf

--- discordbot.py
+++ discordbot.py
@@ -1,4 +1,3 @@
-# These are the dependecies. The bot depends on these to function, hence the name. Please do not change these unless your adding to them, because they can break the bot.
 import discord
 import asyncio
 from discord.ext.commands import Bot
@@ -16,16 +15,19 @@ async def on_ready():
 	print('Use this link to invite {}:'.format(client.user.name))
 	print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
 	print('--------')
-	print('Support Discord Server: https://discord.gg/FNNNgqb')
-	print('Github Link: https://github.com/Habchy/BasicBot')
-	print('--------')
-	print('You are running BasicBot v2.1') #Do not change this. This will really help us support you, if you need support.
-	print('Created by Habchy#1665')
 	return await client.change_presence(game=discord.Game(name='the drums')) #This is buggy, let us know if it doesn't work.
 
 @client.event
 async def on_message(message):
-	if((message.content == "no u") & (message.channel.name == "battle-bots")):
-        	await client.send_message(message.channel, "false, 'tis indeed thou")
-	
+
+	#only work on battle-bots channel
+	if(message.channel.name == "battle-bots") | (message.author.name == "cabobalot"):
+		print(message.author.name + ": " + message.content)
+		if(message.content == "no u") & (message.author.bot == false):
+			await client.send_message(message.channel, "false, 'tis indeed thou.")
+		elif(message.content.lower() == "!setbattle true"):
+			await client.send_message(message.channel, "!setBattle False")
+			await client.send_message(message.channel, "false, 'tis indeed thou.")
+			await client.send_message(message.channel, "You think you can best me? You ninnyhammer!")
+
 client.run('NDEzNDY2MTQxMTg5NzM0NDMw.DWZSnw.W6Di4Rldu70f4SdyORmSvDmVaRE')
